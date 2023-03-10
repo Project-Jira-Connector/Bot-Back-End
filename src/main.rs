df10714f391bd870ec574b24650b689f56fe7ea2
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-03-10 16:56:48 +0700

    Wrap environment variables inside config.

--- src/main.rs
+++ src/main.rs
@@ -7,36 +7,18 @@ async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
-    let bind_addr = std::env::var("BIND_ADDRESS").expect("BIND_ADDRESS must be defined");
-    let bind_port = std::env::var("BIND_PORT")
-        .expect("BIND_PORT must be defined")
-        .parse()
-        .expect("BIND_PORT must be a valid port");
+    let environment = models::config::Environment::new();
+    let client = utils::client::Client::new(
+        &environment.database.username,
+        &environment.database.password,
+    );
 
-    let mongodb_username =
-        std::env::var("MONGODB_USERNAME").expect("MONGODB_USERNAME must be defined");
-    let mongodb_password =
-        std::env::var("MONGODB_PASSWORD").expect("MONGODB_PASSWORD must be defined");
-
-    let schedule: cron::Schedule = std::env::var("SCHEDULE")
-        .expect("SCHEDULE must be defined")
-        .parse()
-        .expect("SCHEDULE must be a valid schedule");
-
-    let notification_email =
-        std::env::var("NOTIFICATION_EMAIL").expect("NOTIFICATION_EMAIL must be defined");
-    let notification_password =
-        std::env::var("NOTIFICATION_PASSWORD").expect("NOTIFICATION_PASSWORD must be defined");
-
-    let client = utils::client::Client::new(&mongodb_username, &mongodb_password);
-
-
-    utils::scheduler::robots(client.clone(), schedule.clone()).await;
+    utils::scheduler::robots(client.clone(), environment.schedule.clone()).await;
     utils::scheduler::purger(
         client.clone(),
-        schedule.clone(),
-        notification_email,
-        notification_password,
+        environment.schedule.clone(),
+        environment.notification.email,
+        environment.notification.password,
     )
     .await;
 
@@ -53,7 +35,7 @@ async fn main() -> std::io::Result<()> {
                     .route(actix_web::web::delete().to(routes::robots::delete)),
             )
     })
-    .bind((bind_addr, bind_port))?
+    .bind((environment.server.address, environment.server.port))?
     .run()
     .await;
 }
